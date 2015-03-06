459d331d06cecd33e079e0a02912c3920ed5b609
aberigle <a.berzosa.iglesias@gmail.com>
2015-03-06 19:40:14 +0100

    new deploy

--- static/js/app.debug.js
+++ static/js/app.debug.js
@@ -2,38 +2,7 @@
   (function() {
     var App;
     App = (function() {
-      function App() {
-        window.onload = (function(_this) {
-          return function() {
-            return _this.init();
-          };
-        })(this);
-      }
-
-      App.prototype.init = function() {
-        var aberigle, letter;
-        aberigle = this.dom("#hero");
-        return aberigle.innerHTML = ((function() {
-          var i, len, ref, results;
-          ref = aberigle.innerHTML.split("");
-          results = [];
-          for (i = 0, len = ref.length; i < len; i++) {
-            letter = ref[i];
-            results.push("<span id=\"" + (letter.toLowerCase()) + "\">" + letter + "</span>");
-          }
-          return results;
-        })()).join("");
-      };
-
-      App.prototype.dom = function(selector) {
-        var items;
-        items = document.querySelectorAll(selector);
-        if (items.length === 1) {
-          return items[0];
-        } else {
-          return items;
-        }
-      };
+      function App() {}
 
       return App;
 
--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,6 @@
 /*
-  app v0.0.0 - 2/3/2015
+  app v0.0.0 - 6/3/2015
 
   Author: _NAME_ - _EMAIL_
 */
-(function(){!function(){var a;return a=function(){function a(){window.onload=function(a){return function(){return a.init()}}(this)}return a.prototype.init=function(){var a,b;return a=this.dom("#hero"),a.innerHTML=function(){var c,d,e,f;for(e=a.innerHTML.split(""),f=[],c=0,d=e.length;d>c;c++)b=e[c],f.push('<span id="'+b.toLowerCase()+'">'+b+"</span>");return f}().join("")},a.prototype.dom=function(a){var b;return b=document.querySelectorAll(a),1===b.length?b[0]:b},a}(),window.App=new a}()}).call(this);
+(function(){!function(){var a;return a=function(){function a(){}return a}(),window.App=new a}()}).call(this);
