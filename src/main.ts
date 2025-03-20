7adedd5bb339ef16f3017484a74ee5f3caa22fcf
aberigle <aberigle@uhurus.com>
2025-03-20 14:17:54 +0100

    animate

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,17 @@ document.querySelectorAll<HTMLElement>(".icon")
     const icon: string = Array
       .from(element.classList)
       .find((item: string) => item !== "icon")!
+
+    setTimeout(() => element.focus(), 3000)
     return element.innerHTML = getSVGIcon(icon) + element.innerHTML
   })
 
+let timeout = 500
 document.querySelectorAll<HTMLSpanElement>("#hero span")
-  .forEach((item: HTMLSpanElement) => {
-    const text = item.innerText == "u"? "uuuu" : item.innerText
-    const message = new SpeechSynthesisUtterance(item.innerText)
-    message.lang = text == "hu"? "en" : "es"
-    return item.onclick = () => speechSynthesis.speak(message)
-  })
+.forEach((item: HTMLSpanElement) => {
+  const text = item.innerText == "u" ? "uuuu" : item.innerText
+  const message = new SpeechSynthesisUtterance(item.innerText)
+  message.lang = text == "hu" ? "en" : "es"
+  setTimeout(() => item.focus(), timeout += 500)
+  return item.onclick = () => speechSynthesis.speak(message)
+})
