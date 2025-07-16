b554134f9a53642435dbafe0dc9628f5ab2fc1f1
aberigle <aberigle@uhurus.com>
2025-07-16 14:03:50 +0200

    add markdownx support

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,20 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   // integrations: [vue()],
   site: "https://uhurus.com",
+
   build: {
     assets: "assets"
   },
 
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [mdx()]
 });
